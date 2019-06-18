9008d9fc22c558327d2d84b9c8fdd0f604fdd7d9
MarcoLight <51535421+MarcoLight@users.noreply.github.com>
2019-06-18 22:22:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class MotorVeicle
+    {
+        public string Targa { get; set; }
+        public int LivelloCarburante { get; set; }
+    }
+
+    class Car : MotorVeicle
+    {
+        public Car(string targa) => Targa = targa;
+
+        public void Acelera()
+        {
+            if (LivelloCarburante == 0)
+            {
+                throw new NotImplementedException("devo ancora implementare l'eccezione customizzata");
+            }
+            else
+            {
+                Console.WriteLine("wwwrrroomm");
+                LivelloCarburante -= 1;
+            }
+        }
+
+    }
+}
